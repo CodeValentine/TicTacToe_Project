e3640cda8186e26b6a47ffc15ae71759d1d490b8
CodeValentine <120920994+CodeValentine@users.noreply.github.com>
2023-02-20 14:42:35 -0700

    Print results messaging logic

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 #Unit1 Project Codecademy, Computer Science Career Path
 #import draw_board function from helpers
-from helpers import draw_board, check_turn
+from helpers import draw_board, check_turn, check_for_win
 import os 
 
 spots = {1 : '1', 2 : '2', 3 : '3', 4 : '4', 5 : '5', 6 : '6', 7 : '7', 8 : '8', 9 : '9'}
 
 playing = True
+complete = False 
 turn = 0
 prev_turn = -1
 
@@ -29,6 +30,20 @@ while playing:
                 #Valid input, update the board
                 turn += 1
                 spots[int(choice)] = check_turn(turn)
+    #Check if the game has ended (and if someone won)
+    if check_for_win(spots): playing, complete = False, True 
+    if turn > 8: playing = False 
 
+#Outside of the loop, print the results
+#Draw the final board
+os.system('cls' if os.name == 'nt' else 'clear')
+draw_board(spots)
+#If there was a winner, say who won
+if complete:
+     if check_turn(turn) == 'X': print("Player 1 Wins!")
+     else: print("Player 2 Wins!")
+else:
+     #Tie Game
+     print("No Winner")
 
-
+print("Thanks for playing!")

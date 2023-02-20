415e69f81d3050c7549d39ac5d2f7c28ccc1e8b0
CodeValentine <120920994+CodeValentine@users.noreply.github.com>
2023-02-20 14:33:56 -0700

    check for win function v1

--- helpers.py
+++ helpers.py
@@ -8,3 +8,20 @@ def check_turn(turn):
     if turn % 2 == 0: return '0'
     else: return 'X'
 
+def check_for_win(spots):
+    #Check Horizontal Cases
+    if (spots[1] == spots[2] == spots[3]) \
+    or (spots[4] == spots[5] == spots[6]) \
+    or (spots[7] == spots[8] == spots[9]): 
+        return True 
+    #Check Vertical Cases
+    if (spots[1] == spots[4] == spots[7]) \
+    or (spots[2] == spots[5] == spots[8]) \
+    or (spots[3] == spots[6] == spots[9]): 
+        return True 
+    # Diagonal Cases
+    elif (spots[1] == spots[5] == spots[9]) \
+    or (spots[3] == spots[5] == spots[7]):
+        return True 
+    
+    else: return False 
